29513beb94a5207866c103de6a555a3c1ee2cd3e
piresgil <piresgildaniel@gmail.com>
2019-10-13 19:02:37 +0200

    Adding database access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -2,9 +2,9 @@ package model.entities;
 
 import java.io.Serializable;
 
-public class Department implements Serializable {
+public class Department implements Serializable{
 	private static final long serialVersionUID = 1L;
-
+	
 	private Integer id;
 	private String name;
 	
